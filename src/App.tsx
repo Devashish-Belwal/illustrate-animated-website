081e77638dec437f543864c676e34295328c74da
Devashish Belwal <433devashish@gmail.com>
2026-09-02 09:19:31 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,241 @@
-import { Button } from "@/components/ui/button"
+import './App.css'
 
 export function App() {
-  return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
+  return (<>
+    <style>
+      {`
+        
+      `}
+    </style>
+    <main>
+      <section
+        className="landing-page"
+      >
+        <section
+          className="top-section"
+        >
+          <nav>
+            <div className="nav-left">
+              <div className="nav-links">
+                {/* <div className="line-div"></div> */}
+                <h3>Home</h3>
+              </div>
+              <div className="nav-links">
+                {/* <div className="line-div"></div> */}
+                <h3>Vision</h3>
+              </div>
+              <div className="nav-links">
+                {/* <div className="line-div"></div> */}
+                <h3>Radiance</h3>
+              </div>
+              <div className="nav-links">
+                {/* <div className="line-div"></div> */}
+                <h3>Gallery</h3>
+              </div>
+              <div className="nav-links">
+                {/* <div className="line-div"></div> */}
+                <h3>Contact</h3>
+              </div>
+            </div>
+            <div className="nav-right">
+              <p>Light shapes every moment, revealing beauty through shadow, warmth, and motion.</p>
+              <p> Discover spaces where brilliance meets simplicity, creating experiences that feel timeless and alive.</p>
+            </div>
+          </nav>
+          <section className="heading">
+            <h1>Illuminate</h1>
+          </section>
+        </section>
+        <section
+          className="bottom-section"
+        >
+          <img
+            src="https://images.unsplash.com/photo-1779401814224-bb9ba82f2846?q=80"
+            alt="Landing Image"
+          />
+        </section>
+      </section>
+      <section
+        className="list-page"
+      >
+        <section className="list-page">
+          <div className="list-div">
+            <div className="list-number">
+              <h2>83</h2>
+            </div>
+            <div className="list-info">
+              <div className="list-description">
+                <p>Soft illumination designed to create warmth, comfort, and a sense of calm.</p>
+                <h2>Glow</h2>
+              </div>
+              <div className="list-img">
+                <img
+                  src="https://images.unsplash.com/photo-1779398970350-14a7222482eb"
+                  alt=""
+                />
+                <img
+                  src="https://images.unsplash.com/photo-1665516597899-597c56f593de"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+          <div className="list-div">
+            <div className="list-number">
+              <h2>07</h2>
+            </div>
+            <div className="list-info">
+              <div className="list-description">
+                <p>Every ray reveals new perspectives, transforming ordinary moments into extraordinary experiences.</p>
+                <h2>Prism</h2>
+              </div>
+              <div className="list-img">
+                <img
+                  src="https://images.unsplash.com/photo-1582672211548-06ef7d3a306a"
+                  alt=""
+                />
+                <img
+                  src="https://images.unsplash.com/photo-1779400882358-8ba06a8e5e11"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+          <div className="list-div">
+            <div className="list-number">
+              <h2>45</h2>
+            </div>
+            <div className="list-info">
+              <div className="list-description">
+                <p>Thoughtfully balanced light that enhances atmosphere, emotion, and everyday living.</p>
+                <h2>Lumen</h2>
+              </div>
+              <div className="list-img">
+                <img
+                  src="https://images.unsplash.com/photo-1667498234868-dfb09fdd5c3b"
+                  alt=""
+                />
+                <img
+                  src="https://images.unsplash.com/photo-1592507195890-fd083a0fdfa8"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+          <div className="list-div">
+            <div className="list-number">
+              <h2>18</h2>
+            </div>
+            <div className="list-info">
+              <div className="list-description">
+                <p>A seamless blend of elegance and brightness, surrounding every space with quiet sophistication.</p>
+                <h2>Halo</h2>
+              </div>
+              <div className="list-img">
+                <img
+                  src="https://images.unsplash.com/photo-1726639348411-2bda16b93def"
+                  alt=""
+                />
+                <img
+                  src="https://images.unsplash.com/photo-1616752720075-3fb2ad02e6b3"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+      </section>
+      <section className="marquee-section">
+        <div className="marquee-track">
+          <img
+            src="https://images.unsplash.com/photo-1772647384177-8b4deb037308"
+            alt=""
+          />
+          <img
+            src="https://images.unsplash.com/photo-1613142890313-a4c4b5d1214f"
+            alt=""
+          />
+          <img
+            src="https://images.unsplash.com/photo-1779401813839-46c50178bc6c"
+            alt=""
+          />
+          <img
+            src="https://images.unsplash.com/photo-1706565029893-da7d5732416c"
+            alt=""
+          />
+          <img
+            src="https://images.unsplash.com/photo-1622756973918-cfdfd8374ed9"
+            alt=""
+          />
+          <img
+            src="https://images.unsplash.com/photo-1711194992360-c0a5eae475bd"
+            alt=""
+          />
         </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
+        <div className="marquee-track">
+          <img
+            src="https://images.unsplash.com/photo-1772647384177-8b4deb037308"
+            alt=""
+          />
+          <img
+            src="https://images.unsplash.com/photo-1613142890313-a4c4b5d1214f"
+            alt=""
+          />
+          <img
+            src="https://images.unsplash.com/photo-1779401813839-46c50178bc6c"
+            alt=""
+          />
+          <img
+            src="https://images.unsplash.com/photo-1706565029893-da7d5732416c"
+            alt=""
+          />
+          <img
+            src="https://images.unsplash.com/photo-1622756973918-cfdfd8374ed9"
+            alt=""
+          />
+          <img
+            src="https://images.unsplash.com/photo-1711194992360-c0a5eae475bd"
+            alt=""
+          />
         </div>
-      </div>
-    </div>
-  )
+      </section>
+      <footer>
+        <section className="footer-top">
+          <div className="footer-top-div">
+            <div className="footer-img-div">
+              <img
+                src="https://images.unsplash.com/photo-1530047139082-5435ca3c4614"
+                alt=""
+              />
+            </div>
+            <div className="footer-description">
+              <p>
+                Every beam carries the power to inspire. From the first light of dawn to the gentle glow of dusk, our vision celebrates illumination as the foundation of beauty and clarity.
+              </p>
+            </div>
+          </div>
+          <div className="footer-top-div">
+            <div className="footer-img-div">
+              <img
+                src="https://images.unsplash.com/photo-1583606784123-7c244f00d29c"
+                alt=""
+              />
+            </div>
+            <div className="footer-description">
+              <p>
+                Light transforms textures, colors, and emotions. Carefully crafted environments embrace natural brightness, creating spaces that feel open, peaceful, and endlessly inviting.
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="footer-bottom">
+          <h2>
+            Illuminate
+          </h2>
+        </section>
+      </footer>
+    </main>
+  </>)
 }
 
 export default App
